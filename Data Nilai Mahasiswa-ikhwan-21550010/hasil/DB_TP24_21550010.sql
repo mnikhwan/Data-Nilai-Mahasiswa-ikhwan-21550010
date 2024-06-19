27aad0ca0a952903cd3d9be3de6c67f5d4af0911
mnikhwan <kibaro.kun21@gmail.com>
2024-06-19 08:18:34 +0700

    uts

---
+++ Data Nilai Mahasiswa-ikhwan-21550010/hasil/DB_TP24_21550010.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS tb_nil_21550010 (nama TEXT, npm TEXT, tugas INTEGER, uts INTEGER, uas INTEGER, akhir INTEGER);
+INSERT INTO tb_nil_21550010 (nama, npm, tugas, uts, uas, akhir) VALUES ('David', '21550009', 78, 85, 97, 296);
+INSERT INTO tb_nil_21550010 (nama, npm, tugas, uts, uas, akhir) VALUES ('aan', '21550010', 20, 17, 45, 76);
+INSERT INTO tb_nil_21550010 (nama, npm, tugas, uts, uas, akhir) VALUES ('aan', '21550010', 20, 45, 17, 118);
